eb42f8ca26d4a8acc8d78f72bba2e02f3619e0e3
wdkawesome <wdkawesome@icloud.com>
2024-08-26 16:21:40 +0200

    Update helloWorld.py

--- task1_project/helloWorld.py
+++ task1_project/helloWorld.py
@@ -3,6 +3,10 @@ print("Git is awesome!")
 
 user_name = input("Enter your name: ")
 user_email = input("Enter your email adress: ")
+location = input("Enter your location: ")
+postal_code = input("Enter your postal code: ")
 
 print(user_name)
 print(user_email)
+print(location)
+print(postal_code)
